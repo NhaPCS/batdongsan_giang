7030b67e029d74527d8a6392345843d5dc392cec
nhapcs <phamnha0209.1995@gmail.com>
2021-05-09 17:32:21 +0700

    finish web

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#docker build -t nhapt/dophong_api .
+# authenticate with the Docker Hub registry
+# push the new Docker image to the Docker registry
+#docker push nhapt/dophong_api
+yarn build
+scp -r build/* root@45.76.180.15:/data/starcity/dist/
