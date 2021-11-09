3b0e4818230ce9bbd62aa33c1b3f566efd0dd1ba
Nah Pham <nah.p@evano.com>
2021-11-09 18:09:56 +0700

    add bid

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
 # push the new Docker image to the Docker registry
 #docker push nhapt/dophong_api
 yarn build
-scp -r build/* root@45.76.180.15:/data/starcity/dist/
+scp -r build/* root@45.76.180.15:/data/bid/dist/
